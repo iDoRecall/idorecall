506a948374c7255406c723abe42a0774aa94a628
Kyrylo Chernov <kirill.chernov@hygge.software>
2023-06-06 16:50:34 +0300

    IDRT-1763: Added Logo component;

---
+++ src/components/atoms/Logo/component.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './styles.scss';
+
+export const Logo = () => {
+    return (
+        <div className='logoContent'>
+            <div className='wrapImg'>
+                <img
+                    src={'assets/images/sync-icon.svg'}
+                    alt='logo'
+                />
+            </div>
+            <div className='wrapTitle'>
+                <strong>iDoRecall</strong>
+                <div>obsidian</div>
+            </div>
+        </div>
+    )
+}
