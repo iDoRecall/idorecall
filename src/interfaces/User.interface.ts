e3080c80f6ff582e0039678f9eae59695c284ede
Kyrylo Chernov <kirill.chernov@hygge.software>
2023-06-06 16:35:00 +0300

    IDRT-1763: Added User interface

---
+++ src/interfaces/User.interface.ts
@@ -0,0 +1,24 @@
+export interface User {
+    objectIdString: string;
+    avatarUrl: string;
+    email: string;
+    firstName: string;
+    id: string;
+    unprocessedLibraryItems?: number;
+    timezone: number;
+    createdAt: Date;
+    updatedAt: Date;
+    avatar: string | File;
+    lastName: string;
+    isPushSubscribe: boolean;
+    pushRemindersDays: number[];
+    isEmailSubscribe: boolean;
+    emailRemindersDays: number[];
+    weekendsRemindersTime: string;
+    weekdaysRemindersTime: string;
+    cloneBySomeone: boolean;
+    actionForAdmin: boolean;
+    inviteId: string;
+    recallsQuestionAutoplayState: number;
+    recallsAnswerAutoplayState: number;
+}
