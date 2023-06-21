d4460a4ee804c20a705144767b4cdf3b01918f7d
Kyrylo Chernov <kirill.chernov@hygge.software>
2023-06-21 14:21:51 +0300

    IDRT-1763: Routing updated with user loading conditional routes

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,16 @@ import {
 } from 'react-router-dom';
 import { Home } from './components/pages/Home';
 import { EditRecall } from './components/pages/EditRecall';
+import { useUserState } from './states/useUserState';
+import { Loading } from './components/pages/Loading';
 
 export const App = () => {
+    const isUserLoading = useUserState((state) => state.isLoading);
+
+    if (isUserLoading) {
+        return <Loading />;
+    }
+
     return (
         <Router>
             <Routes>
