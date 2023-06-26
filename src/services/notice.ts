1d1f6261f63f71ed409f90f4f1294ebd691172a8
Kyrylo Chernov <kirill.chernov@hygge.software>
2023-06-26 13:57:52 +0300

    IDRT-1763: notice service added

---
+++ src/services/notice.ts
@@ -0,0 +1,16 @@
+import { Notice } from 'obsidian';
+
+export class NoticeService {
+    private static _instance: NoticeService;
+
+    public notice(text: string | DocumentFragment, duration?: number): void {
+        new Notice(text, duration);
+    }
+
+    public static get instance(): NoticeService {
+        if (!NoticeService._instance) {
+            NoticeService._instance = new NoticeService();
+        }
+        return NoticeService._instance;
+    }
+}
