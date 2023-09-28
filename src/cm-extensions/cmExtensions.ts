95acda446b41c2b43a7a85e87a149eca94377498
Aleksandr Zhukovskiy <aleksandr.zhukovskiy@hygge.software>
2023-09-28 14:15:36 +0200

    Add LICENCE, resolved PR comments

--- src/cm-extensions/cmExtensions.ts
+++ src/cm-extensions/cmExtensions.ts
@@ -1,15 +1,15 @@
-import IDRPlugin from 'main'
-import { cursorTooltip } from './inlineStylerView/inlineStyler'
-import { toggleMarkExtension } from './inlineStylerView/marks'
-import { tooltips } from './tooltip'
+import IDRPlugin from 'main';
+import { cursorTooltip } from './inlineStylerView/inlineStyler';
+import { toggleMarkExtension } from './inlineStylerView/marks';
+import { tooltips } from '@codemirror/view';
 
 export const cmExtensions = (plugin: IDRPlugin) => {
-    let extensions = []
+    const extensions = [];
 
     extensions.push(
-        ...[toggleMarkExtension, tooltips({ parent: document.body })]
-    )
-    extensions.push(cursorTooltip(plugin))
+        ...[toggleMarkExtension, tooltips({ parent: document.body })],
+    );
+    extensions.push(cursorTooltip(plugin));
 
-    return extensions
-}
+    return extensions;
+};
--- src/constants/command-list.ts
+++ src/constants/command-list.ts
@@ -11,21 +11,17 @@ export const COMMAND_LIST: Command[] = [
     {
         id: COMMAND_ID.OPEN_PLUGIN,
         name: 'Open plugin',
-        hotkeys: [{ modifiers: ['Mod'], key: 't' }],
     },
     {
         id: COMMAND_ID.CREATE_RECALL_ANSWER,
         name: 'Create recall, send selected text to the A',
-        hotkeys: [{ modifiers: ['Mod'], key: 'a' }],
     },
     {
         id: COMMAND_ID.CREATE_RECALL_QUESTION,
         name: 'Create recall, send selected text to the Q',
-        hotkeys: [{ modifiers: ['Mod'], key: 'q' }],
     },
     {
         id: COMMAND_ID.CREATE_RECALL_QUESTION_AND_ANSWER,
         name: 'Create recall, send selected text to both the Q and A',
-        hotkeys: [{ modifiers: ['Mod'], key: 'r' }],
     },
 ];
